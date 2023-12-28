e445af7449fe19ee94b0f35fa56d24ac38ece8e2
Sonal Agrawal <sonalfeb7@gmail.com>
2023-12-28 18:09:33 +0530

    Added Functionality of Increment /Decrement Counter in Redux

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const redux = require("redux");
-
 const CounterReducer = (
   initialState = {
     counter: 0
   },
   action
 ) => {
-  return {
-    counter: initialState.counter + 1
-  };
+  if (action.type == "increment") {
+    return {
+      counter: initialState.counter + 1
+    };
+  }
+  if(action.type=='decrement'){
+    return{
+        counter: initialState.counter - 1
+    }
+  }
 };
 
 const store = redux.createStore(CounterReducer);
@@ -17,11 +23,5 @@ const CounterSubcription = () => {
   console.log(store.getState());
 };
 store.subscribe(CounterSubcription);
-store.dispatch({type:'increment'});
-store.dispatch({type:'increment'});
-
-store.dispatch({type:'increment'});
-store.dispatch({type:'increment'});
-
-store.dispatch({type:'increment'});
-store.dispatch({type:'increment'});
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
